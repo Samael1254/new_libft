dfa18c2a392cd65bac57701263a5c0bfb9abd005
gfulconi <gfulconi@student.42.fr>
2025-01-03 10:05:41 +0100

    modified ft_list_index

--- src/lists/ft_list_index.c
+++ src/lists/ft_list_index.c
@@ -6,7 +6,7 @@
 /*   By: gfulconi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 11:51:32 by gfulconi          #+#    #+#             */
-/*   Updated: 2024/12/05 11:53:32 by gfulconi         ###   ########.fr       */
+/*   Updated: 2024/12/23 11:00:15 by gfulconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	ft_list_index(t_list *begin_list, t_list *elem)
 	int	i;
 
 	i = 0;
-	while (begin_list && begin_list != elem)
+	while (begin_list)
 	{
+		if (begin_list == elem)
+			return (i);
 		i++;
 		begin_list = begin_list->next;
 	}
-	if (begin_list == elem)
-		return (i);
 	return (-1);
 }
