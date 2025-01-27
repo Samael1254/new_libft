04a6c134a077ffb4ae8392cf4b22e3eb20b3c307
Tom Le Prigent <tle-prig@paul-f4Ar6s5.clusters.42paris.fr>
2025-01-27 14:48:04 +0100

    fixed return if malloc fail on ft_list_push_front

--- src/lists/ft_list_push_front.c
+++ src/lists/ft_list_push_front.c
@@ -12,13 +12,18 @@
 
 #include "ft_list.h"
 
-void	ft_list_push_front(t_list **begin_list, void *data)
+int	ft_list_push_front(t_list **begin_list, void *data)
 {
 	t_list	*temp;
-
+	t_list	*new_elem;
+	
+	new_elem = ft_create_elem(data);
+	if (!new_elem)
+		return (1);
 	temp = *begin_list;
-	*begin_list = ft_create_elem(data);
+	*begin_list = new_elem;
 	(*begin_list)->next = temp;
+	return (0);
 }
 
 // #include <stdio.h>
