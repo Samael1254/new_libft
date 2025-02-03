9600a8911a96acdbb0dc8eb7222db62fa042f9b4
gfulconi <gfulconi@student.42.fr>
2025-02-03 13:30:30 +0100

    updated ft_str_is_number to accept leading +

--- src/strings/ft_str_is_number.c
+++ src/strings/ft_str_is_number.c
@@ -6,7 +6,7 @@
 /*   By: gfulconi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 10:59:32 by gfulconi          #+#    #+#             */
-/*   Updated: 2024/11/20 10:50:22 by gfulconi         ###   ########.fr       */
+/*   Updated: 2025/02/03 13:29:53 by gfulconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_str_is_number(const char *str)
 	if (!str[0])
 		return (0);
 	j = 0;
-	if (str[0] == '-' && str[1])
+	if ((str[0] == '-' || str[0] == '+') && str[1])
 		j = 1;
 	while (str[j])
 	{
