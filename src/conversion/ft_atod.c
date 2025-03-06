b3288babf352e50ef128545ec3cc8e01e80eff6b
maranici <macuesta@student.42.fr>
2025-03-06 14:56:08 +0100

    ft_atod without any malloc, better performance, less risks

--- src/conversion/ft_atod.c
+++ src/conversion/ft_atod.c
@@ -3,26 +3,21 @@
 
 /*
  * returns a double from a string, returns 0 on error
+ * find the dot in the char *, return the int instant if not found
  * */
 double	ft_atod(const char *nptr)
 {
 	double	integer;
 	double	decimal;
-	char	**split;
 	int		decimal_len;
+	char	*ptr_dot;
 
-	split = ft_split(nptr, '.');
-	if (!split || !*split)
-		return (0);
-	integer = (double)ft_atoi(split[0]);
-	if (ft_strtab_size(split) < 2)
-	{
-		ft_free_strtab(split);
+	ptr_dot = ft_strchr(nptr, '.');
+	integer = (double)ft_atoi(nptr);
+	if (!ptr_dot)
 		return (integer);
-	}
-	decimal = (double)ft_atoi(split[1]);
-	decimal_len = ft_strlen(split[1]);
-	ft_free_strtab(split);
+	decimal = (double)ft_atoi(ptr_dot + 1);
+	decimal_len = ft_strlen(ptr_dot + 1);
 	while (decimal_len--)
 		decimal /= 10;
 	return (integer + decimal);
