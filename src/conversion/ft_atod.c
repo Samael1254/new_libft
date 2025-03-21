50b902b459f1750e4fc03a6011f4de245f5ea870
gfulconi <fulconis.guillaume.foe@gmail.com>
2025-03-21 18:00:03 +0100

    fixed atod again

--- src/conversion/ft_atod.c
+++ src/conversion/ft_atod.c
@@ -1,3 +1,4 @@
+#include "ft_chars.h"
 #include "ft_conversion.h"
 #include "ft_math.h"
 #include "ft_strings.h"
@@ -11,12 +12,17 @@ double	ft_atod(const char *nptr)
 	double	integer;
 	double	decimal;
 	char	*ptr_dot;
+	int		sign;
 
+	while (ft_isspace(*nptr))
+		nptr++;
+	if (*nptr)
+		sign = 1 - 2 * (*nptr == '-');
 	integer = (double)ft_atoi(nptr);
 	ptr_dot = ft_strchr(nptr, '.');
 	if (!ptr_dot)
 		return (integer);
 	decimal = (double)ft_atoi(ptr_dot + 1);
 	decimal /= ft_powerf(10, ft_strspn(ptr_dot + 1, "0123456789"));
-	return (integer + (2 * (integer > 0) - 1) * decimal);
+	return (integer + sign * decimal);
 }
